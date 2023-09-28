fe6ea6a980fa9b34e4f0dd05d35de457690ad25a
schwarzam <gustavo.b.schwarz@gmail.com>
2023-09-28 19:36:47 -0300

    messing with virtualsky

--- frontend/src/VirtualSkyComponent.js
+++ frontend/src/VirtualSkyComponent.js
@@ -4,17 +4,33 @@ import { useEffect } from "react";
 
 const VirtualSkyComponent = props => {
     const id = "test";
-    const width = "1000px";
-    const height = "1000px";
+    const width = "700px";
+    const height = "700px";
   
     useEffect(() => {
       const planetarium = window.S.virtualsky({ 
         id,
-        projection: 'polar',
-		    objects: 'messier.json',
+        projection: 'fisheye',
+		    //objects: 'messier.json',
         latitude: -22.9712,
-        longitude: -46.9964,
-      
+        longitude: -90,
+        az: 180,
+        live: true,
+        gridlines_eq: true,
+        negative: false,
+        mouse: false,
+        ground: true,
+        magnitude: 10, 
+        showplanets: true,
+        showstarlabels: true,
+        transparent: true,
+
+
+        callback: {
+          click: function(e) {
+            console.log(e.ra, e.dec);
+          }
+        }
       });
     }, []);
   
